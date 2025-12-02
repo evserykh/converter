68ba09d151933a1bde267cdeb91bb96d2d2c8f27
Konstantin Ivanov <kewogc@gmail.com>
2025-12-02 18:17:24 +0700

    fix docker files and wrong 8100 replaced port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-/usr/bin/soffice --headless --nofirststartwizard --accept='socket,host=127.0.0.1,port=8080;urp' & > /dev/null 2>&1
+# Run LibreOffice in background
+/usr/bin/soffice --headless --nofirststartwizard --accept='socket,host=127.0.0.1,port=8100;urp' > /dev/null 2>&1 &
+
+# pause before initialize LibreOffice
+sleep 2
+
+# Run Tomcat with JodConverter
 /opt/apache-tomcat-8.0.26/bin/catalina.sh start > /dev/null 2>&1
+
+# Pause for initialize Tomcat
+sleep 3
 
 exec gosu ruby "${@}"
